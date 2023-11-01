315fb7c5dc93ac19c1c75f09b96c9ce318bf3161
Jared Burns <burnsja@vmware.com>
2023-11-01 16:46:06 -0400

    docs:/air-gapped
    Created Documentation for Air-gapped Machines.
    
    Signed-off-by: Jared Burns <burnsja@vmware.com>

---
+++ docs/snippets/save-module-local-windows.ps1
@@ -0,0 +1,4 @@
+Save-Module -Name VMware.PowerCLI -MinimumVersion 13.1.0 -Path F:\Module\
+Save-Module -Name PowerVCF -MinimumVersion 2.4.0 -Path F:\Module\
+Save-Module -Name PowerValidatedSolutions -MinimumVersion 2.6.0 -Path F:\Module\
+Save-Module -Name VMware.CloudFoundation.CertificateManagement -Path F:\Module\
